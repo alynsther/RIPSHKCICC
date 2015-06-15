4862404de1880e9db11b3b8a3d575fc5aa7d230c
jrt54 <jrt5493@gmail.com>
2015-06-15 11:14:12 +0800

    now returns the list from function call
    call listofprices() to call the random walk
    
    soon to be added: easy way to call it and change the parameters for
    number of time steps, volatility, and drift during function call

--- randomwalk.py
+++ randomwalk.py
@@ -1,31 +1,40 @@
-import math
-
-initialprice = 1
-#drift and volatility over full time period
-percentagedrift = .05
-percentvolatility = .1
-newprice = initialprice
-#number of iterations of a given timestep
-n = 20 
-x = 0.0
-#what portion of a full period is our timestep
-dt = 0.1
-
 
 
 
 #http://wiki.scipy.org/Cookbook/BrownianMotionhttp://wiki.scipy.org/Cookbook/BrownianMotion
 
+import math
 from scipy.stats import norm 
 # Process parameters
 
 # Initial condition.
 x = 0.0 
 # Number of iterations to compute.
-
+def listofprices():
 # Iterate to compute the steps of the Brownian motion.
-for k in range(n):
-	x = x + norm.rvs(scale=percentvolatility**2*dt)
-	newprice = newprice*math.exp((percentagedrift - (percentvolatility**2)/2)+percentvolatility*x)
-	print x 
-	print("Stock price", newprice)
+# Process parameters
+# Initial condition.
+	initialprice = 1
+#drift and volatility over full time period
+	percentagedrift = .05
+	percentvolatility = .1
+	newprice = initialprice
+
+#number of iterations of a given timestep
+	n = 20 
+	stockprices = [0]*n
+#x is weiner process and always starts at 0
+	x = 0.0
+	#what portion of a full period is our timestep
+	dt = 0.1
+	for k in range(n):
+		x = x + norm.rvs(scale=percentvolatility**2*dt)
+		newprice = newprice*math.exp((percentagedrift - (percentvolatility**2)/2)+percentvolatility*x)	
+		#print("Random Weiner Process", x) 
+		#print("Stock price", newprice)
+		stockprices[k] = newprice
+	return stockprices
+		
+
+stocklist = listofprices()
+print(stocklist)
