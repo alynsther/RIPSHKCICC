8ffc5b204afaab8d6fe616ae4de5df83fbf1b0cb
xlibe <xlibe@connect.ust.hk>
2015-06-29 09:23:05 +0800

    lol

--- frequencyVectors.py
+++ frequencyVectors.py
@@ -8,7 +8,10 @@ def countWords(strlist):
         for i in range(len(strlist[j])):
             if strlist[j][i] not in worep:
                 worep+=strlist[j][i]
-    print worep
+    return worep
+
+def countOccur(strlist):
+    worep=countWords(strlist)
     occurrences=[]
     for i in range(len(strlist)):
         occurrences.append([])
@@ -20,18 +23,40 @@ def countWords(strlist):
             occurrences[i][j] = occurrences[i][j]/total
     return occurrences
 
-string=u"我起来我们困"
-string2=u"他他"
-string3=u"ad"
-strlist=[string,string2,string3]
-
-def calculateCov(frequencyVectors): #,priceVector):
+def calculateCorr(frequencyVectors,priceVector):
     a=[]
     for i in range(len(frequencyVectors)):
         a.append(frequencyVectors[i])
-    #a.append(priceVector)
     x=numpy.array(a).T
-    y=numpy.cov(x).tolist()
+    a=x.tolist()
+    a.append(priceVector)
+    z=numpy.array(a)
+    y=numpy.ma.corrcoef(z).tolist()
     length=len(y)
     return y[length-1]
-print calculateCov(countWords(strlist))
+
+# this is without selection return every word and its correlation coefficient
+'''def frequencyDict(strlist,price):
+    worep=countWords(strlist)
+    corrcoef=calculateCorr(countOccur(strlist),price)
+    dic={}
+    for i in range(len(worep)):
+        dic[worep[i]]=corrcoef[i]
+    return dic'''
+
+# this selects what is believed to be correlated
+def corrDict(strlist,price):
+    worep=countWords(strlist)
+    corrcoef=calculateCorr(countOccur(strlist),price)
+    dic={}
+    for i in range(len(worep)):
+        if corrcoef[i] != None and abs(corrcoef[i])>0.6:
+            dic[worep[i]]=corrcoef[i]
+    return dic
+
+strlist=[u"陈陈一轮",u"陈二轮",u"陈三轮轮轮",u"陈四轮",u"陈五轮"]
+
+print countWords(strlist)
+print countOccur(strlist)
+print calculateCorr(countOccur(strlist),[0.2,-0.1,-0.05,0.05,0.1])
+print corrDict(strlist,[0.2,-0.1,-0.05,0.05,0.1])
