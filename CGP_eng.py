14f377cc0045b9d379d65bb7d5ec2c5ecc12420f
xlibe <xlibe@connect.ust.hk>
2015-06-29 15:37:47 +0800

    lol

--- CGP_eng.py
+++ CGP_eng.py
@@ -1,15 +1,14 @@
 # encoding=utf8
 
 import nltk
-import turtle
+
 from nltk.tokenize import sent_tokenize, word_tokenize
-import re
+
 
 letters = {}
 
-currentPosition = [0,0,0,0,0]
 
-positionList = []
+
 
 letterPositions={"a":[0,0,0,0,1],"b":[0,0,0,1,0],"c":[0,0,0,1,1],"d":[0,0,1,0,0],"e":[0,0,1,0,1],
                  "f":[0,0,1,1,0],"g":[0,0,1,1,1],"h":[0,1,0,0,0],"i":[0,1,0,0,1],"j":[0,1,0,1,0],
@@ -38,17 +37,32 @@ def import_from_txt():
 	return text
 
 def move_points(string):
-	for j in range(len(string)):
-		k = string[j]
-		for i in range(5):
-			currentPosition[i] = 0.5 * float(int(letterPositions[k][i]) + float(currentPosition[i]))
-		positionList.append(currentPosition)
+        positionList = [[0,0,0,0,0]]
+        for j in range(len(string)):
+                currentPosition=[]
+                for i in range(5):
+                        currentPosition.append(0.5 * float(int(letterPositions[string[j]][i]) + float(positionList[j][i])))
+                positionList.append(currentPosition)
+        positionList.remove(positionList[0])
+        return positionList
 
 def processing(positionList):
-	pass
+        dic={}
+        for i in range(3):
+                for j in range(3):
+                        for k in range(3):
+                                for l in range(3):
+                                        for m in range(3):
+                                                dic[(i,j,k,l,m)]=0
+        for point in positionList:
+                dic[(int(point[0]*3)),int(point[1]*3),int(point[2]*3),int(point[3]*3),int(point[4]*3)]+=1
+        return dic
 
 letter = remove_punc(import_from_txt())
-move_points(letter)
+positionList=move_points(letter)
+
+print processing(positionList)
+print sum(processing(positionList).values())
 
 print positionList
 
