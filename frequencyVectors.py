dd4bed2858a4fa601c1cc6697ce24f526ee0f2a7
xlibe <xlibe@connect.ust.hk>
2015-06-29 10:02:05 +0800

    lol

--- frequencyVectors.py
+++ frequencyVectors.py
@@ -2,6 +2,25 @@
 
 import numpy
 
+alist = [[],[],[],[],[]]
+
+string_list = []
+string = ''
+
+
+punctuations = [u'，',u'。',u'？',u'！',u'；',u'：',u' ',u'.',u'(',u'、',u'（',u'）',u')',u'-']
+
+for i in range(5):
+    alist[i] = open('sample' + str(i) + '.txt', 'r')
+    string=""
+    for line in alist[i]:
+        u_line = unicode(line, 'gbk')
+        for j in range(len(punctuations)):
+            u_line = u_line.replace(punctuations[j], '')
+
+        string += u_line.rstrip()
+        string_list.append(string)
+
 def countWords(strlist):
     worep=""
     for j in range(len(strlist)):
@@ -13,6 +32,7 @@ def countWords(strlist):
 def countOccur(strlist):
     worep=countWords(strlist)
     occurrences=[]
+    frequencyVector=[]
     for i in range(len(strlist)):
         occurrences.append([])
     for i in range(len(strlist)):
@@ -21,6 +41,8 @@ def countOccur(strlist):
             total=float(sum(occurrences[i]))
         for j in range(len(occurrences[i])):
             occurrences[i][j] = occurrences[i][j]/total
+        frequencyVector.append(dict(zip(worep, occurrences[i])))
+    print frequencyVector
     return occurrences
 
 def calculateCorr(frequencyVectors,priceVector):
@@ -54,9 +76,9 @@ def corrDict(strlist,price):
             dic[worep[i]]=corrcoef[i]
     return dic
 
-strlist=[u"陈陈一轮",u"陈二轮",u"陈三轮轮轮",u"陈四轮",u"陈五轮"]
+#string_list=[u"陈陈一轮",u"陈二轮",u"陈三轮轮轮",u"陈四轮",u"陈五轮"]
 
-print countWords(strlist)
-print countOccur(strlist)
-print calculateCorr(countOccur(strlist),[0.2,-0.1,-0.05,0.05,0.1])
-print corrDict(strlist,[0.2,-0.1,-0.05,0.05,0.1])
+print countWords(string_list)
+#print countOccur(string_list)
+#print calculateCorr(countOccur(string_list),[0.2,-0.1,-0.05,0.05,0.1])
+print corrDict(string_list,[0.2,-0.1,-0.05,0.05,0.1])
