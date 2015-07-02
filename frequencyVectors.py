25ec531436a74e86349b1a87241331581c05ccbc
xlibe <xlibe@connect.ust.hk>
2015-07-02 10:06:54 +0800

    lol

--- frequencyVectors.py
+++ frequencyVectors.py
@@ -1,6 +1,8 @@
 #encoding=utf8
 
-import numpy
+import numpy    #this module is used in calculating correlation coefficient matrix
+
+import uniout   #this module is used to directly print chinese characters instead of the codes
 
 alist = [[],[],[],[],[]]
 
@@ -10,40 +12,46 @@ string = ''
 
 punctuations = [u'，',u'。',u'？',u'！',u'；',u'：',u' ',u'.',u'(',u'、',u'（',u'）',u')',u'-']
 
-for i in range(5):
+for i in range(5):  #this loop reads texts from different files and put them into a list of strings
     alist[i] = open('sample' + str(i) + '.txt', 'r')
     string=""
-    for line in alist[i]:
-        u_line = unicode(line, 'gbk')
+    for line in alist[i]:   #this loop read lines from one single file and put them in one string
+        u_line = unicode(line, 'utf8')
         for j in range(len(punctuations)):
             u_line = u_line.replace(punctuations[j], '')
 
         string += u_line.rstrip()
-        string_list.append(string)
+        string=string[1:]   #in Windows I need this to read text from notepad, but in Mac, Allen doesn't need this
+    string_list.append(string)
+print string_list
 
-def countWords(strlist):
-    worep=""
-    for j in range(len(strlist)):
-        for i in range(len(strlist[j])):
-            if strlist[j][i] not in worep:
-                worep+=strlist[j][i]
-    return worep
 
-def countOccur(strlist):
-    worep=countWords(strlist)
-    occurrences=[]
-    frequencyVector=[]
+def countWords(strlist,windowLength):
+    worep=[]    #worep means w/o repetition, it contains all different phrases which appear in at least one file
+    for j in range(len(strlist)):   #loop through all strings
+        for i in range(len(strlist[j])-int(windowLength) + 1):  #loop through all phrases of a single string
+            if strlist[j][i:i+int(windowLength)] not in worep:  #here the phrase it 3-character long, but the length can be changed
+                worep.append(strlist[j][i:i + int(windowLength)])
+    return worep   # it is a string contains all different phrases
+
+def countOccur(strlist,windowLength):
+    worep=countWords(strlist,windowLength)
+    frequency=[]    #this is a list of lists which contain the frequency of different words in one day
+    frequencyVector=[]  #this is a list of  dictionaries, key is the phrase and value is the frequency of different phrases in one day
+    countVector=[]  #this is a list of  dictionaries, key is the phrase and value is the number of different phrases in one day
     for i in range(len(strlist)):
-        occurrences.append([])
+        frequency.append([])
     for i in range(len(strlist)):
         for j in range(len(worep)):
-            occurrences[i].append(strlist[i].count(worep[j]))
-            total=float(sum(occurrences[i]))
-        for j in range(len(occurrences[i])):
-            occurrences[i][j] = occurrences[i][j]/total
-        frequencyVector.append(dict(zip(worep, occurrences[i])))
+            frequency[i].append(strlist[i].count(worep[j]))
+            total=float(sum(frequency[i]))
+        countVector.append(dict(zip(worep, frequency[i])))
+        for j in range(len(frequency[i])):
+            frequency[i][j] = frequency[i][j]/total
+        frequencyVector.append(dict(zip(worep, frequency[i])))
+    print countVector
     print frequencyVector
-    return occurrences
+    return frequency   # we only pass the list i.e. the value of frequency to the next function
 
 def calculateCorr(frequencyVectors,priceVector):
     a=[]
@@ -53,9 +61,9 @@ def calculateCorr(frequencyVectors,priceVector):
     a=x.tolist()
     a.append(priceVector)
     z=numpy.array(a)
-    y=numpy.ma.corrcoef(z).tolist()
+    y=numpy.ma.corrcoef(z).tolist()   #from numpy module, this method can calculate the correlation coefficients of an array and returns a list
     length=len(y)
-    return y[length-1]
+    return y[length-1]   # we only want the item of the list, which contains the corr between phrase frequency and stock prices 
 
 # this is without selection return every word and its correlation coefficient
 '''def frequencyDict(strlist,price):
@@ -66,19 +74,22 @@ def calculateCorr(frequencyVectors,priceVector):
         dic[worep[i]]=corrcoef[i]
     return dic'''
 
-# this selects what is believed to be correlated
-def corrDict(strlist,price):
-    worep=countWords(strlist)
-    corrcoef=calculateCorr(countOccur(strlist),price)
+# this selects what is believed to be correlated and absolute value of corr must be greater than 0.8, this value can be changed
+def corrDict(strlist,price,windowLength):
+    worep=countWords(strlist,windowLength)
+    corrcoef=calculateCorr(countOccur(strlist,windowLength),price)
     dic={}
     for i in range(len(worep)):
-        if corrcoef[i] != None and abs(corrcoef[i])>0.6:
+        if corrcoef[i] != None and abs(corrcoef[i])>0.8:
             dic[worep[i]]=corrcoef[i]
     return dic
 
-#string_list=[u"陈陈一轮",u"陈二轮",u"陈三轮轮轮",u"陈四轮",u"陈五轮"]
+price=[80.44,88.83,87.82,87.08,87.74]   # type in the price list
+windowLength=raw_input("Input the window length you want to use: ")   #input the window length
 
-print countWords(string_list)
-#print countOccur(string_list)
-#print calculateCorr(countOccur(string_list),[0.2,-0.1,-0.05,0.05,0.1])
-print corrDict(string_list,[0.2,-0.1,-0.05,0.05,0.1])
+#print countWords(string_list,windowLength)
+#print countOccur(string_list,windowLength)
+#print calculateCorr(countOccur(string_list,windowLength),price)
+
+
+print corrDict(string_list,price,windowLength)
