abc078443cef7ca0d1869a79c0603554c9484805
jrt54 <jrt5493@gmail.com>
2015-07-13 15:16:57 +0800

    almost done

--- souppractice.py
+++ souppractice.py
@@ -8,51 +8,45 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 #open the article
-soup = BeautifulSoup(urllib2.urlopen('http://www.businessweek.com/archive/2015-02/bbg.html').read())
+soup = BeautifulSoup(urllib2.urlopen('http://www.wsj.com/public/page/archive-2015-5-13.html').read())
 #print(soup)
 #find things with time tag and attribute 'datetime.' doesn't work for all articles
-for i in soup.findAll('time'):
-        if i.has_attr('datetime'):
-            print(i['datetime'])
+# for i in soup.findAll('time'):
+#         if i.has_attr('datetime'):
+#             print(i['datetime'])
 
 
 
 
 
 
-# #finds all the links
-# list_of_hyperlinks = ["www.bloomberg.com"]
-# for i in soup.findAll('a'):
-# 	if i.has_attr('href'):
-# 		s =  i['href']
-# 		# s =  "WWW.BLOOMBERG.COM" + i['href']
-# 		goodstring = False
-# 		 #cleans up the useless strings
-# 		for i in range(len(s)):
-# 			if s[i:i+4] == "news":
-# 				goodstring = True
-# 		if(goodstring):
-# 			list_of_hyperlinks.append(s)
+#finds all the links
+list_of_hyperlinks = ["google.com"]
+for i in soup.findAll('a'):
+	if i.has_attr('href'):
+		s =  i['href']
+		goodstring = False
+		 #cleans up the useless strings
+		for i in range(len(s)):
+			if s[i:i+8] == "articles":
+				goodstring = True
+		if(goodstring):
+			list_of_hyperlinks.append(s)
 
 
-# print(set(list_of_hyperlinks))
-
-urllib.urlretrieve("http://www.wsj.com/articles/provocateur-j-j-brine-stirs-up-new-york-art-scene-1431465492", "foobar.html")
-html = open("foobar.html").read()
-s = html2text.html2text(html)
-print(s)
-
-#bug: regardless of character, prints the same results. 
-starting_place = s.find("COMMENTS")
-ending_place = s.find("**Write to", starting_place)
-print(starting_place)
-print(ending_place)
-
-print(s[starting_place:ending_place])
-
-
-f = open("test.txt", "w")
-f.write(s[starting_place:ending_place])
+print(list_of_hyperlinks)
+print(len(list_of_hyperlinks))
+for i in range(1, len(list_of_hyperlinks)):
+	urllib.urlretrieve(list_of_hyperlinks[i], "foobar.html")
+	html = open("foobar.html").read()
+	s = html2text.html2text(html)
+	starting_place = s.find("Updated")
+	ending_place = s.find("To Read the Full Story", starting_place)
+	if starting_place != -1 and ending_place !=1:
+		print(s[starting_place:ending_place])
+		f = open("test513.txt", "a")
+		f.write(s[starting_place:ending_place])
+		f.close
 
 
 
