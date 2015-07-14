2ba303b8f31ea88973221482333d67ecaa8d40cd
jrt54 <jrt5493@gmail.com>
2015-07-14 17:49:36 +0800

    2009-2010 data

--- wallstreetjournal.py
+++ wallstreetjournal.py
@@ -33,25 +33,24 @@ index_of_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 index_of_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 #soup = BeautifulSoup(urllib2.urlopen('http://www.businessweek.com/archive/2009-01/bbg/day1.html').read())
-for year in range (2010, 2011):
-	#for month in range (9, 12):
-	month = 8
-	for day in range(1, index_of_days[month]+1):
-		urllib.urlretrieve('http://www.businessweek.com/archive/' + str(year)+ '-' +str((month+1)).zfill(2)+'/bbg/day' + str(day)+'.html', "foobar.html")
-		html = open("foobar.html").read()
-		s = html2text.html2text(html)
-		starting_search = s.find("#### " + index_of_months[month]  +" " + str(day).zfill(2) + ", " + str(year))
-		starting_place = s.find("[", starting_search)
-		ending_place = s.find("]", starting_search)
-		while(ending_place < s.find("Terms of Service")):
-			print(s[starting_place+1 :ending_place])
-			f = open("bloomberg_" + str(year) + "_" + str(month+1) + "_"+ str(day), "a")
-			f.write(s[starting_place+1:ending_place])
-			f.write('\n')
-			f.close
-			starting_search = ending_place + 1
+for year in range (2009, 2010):
+	for month in range (0, 12):
+		for day in range(1, index_of_days[month]+1):
+			urllib.urlretrieve('http://www.businessweek.com/archive/' + str(year)+ '-' +str((month+1)).zfill(2)+'/bbg/day' + str(day)+'.html', "foobar.html")
+			html = open("foobar.html").read()
+			s = html2text.html2text(html)
+			starting_search = s.find("#### " + index_of_months[month]  +" " + str(day).zfill(2) + ", " + str(year))
 			starting_place = s.find("[", starting_search)
 			ending_place = s.find("]", starting_search)
-			
+			while(ending_place < s.find("Terms of Service")):
+				print(s[starting_place+1 :ending_place])
+				f = open("bloomberg_" + str(year) + "_" + str(month+1) + "_"+ str(day) + ".txt", "a")
+				f.write(s[starting_place+1:ending_place])
+				f.write('\n')
+				f.close
+				starting_search = ending_place + 1
+				starting_place = s.find("[", starting_search)
+				ending_place = s.find("]", starting_search)
+				
 
 				
